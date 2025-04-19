904cc35edc6c4bd1588cb66cc848992c98507b51
NISCHAY HR <nischayhr11@gmail.com>
2025-04-19 22:59:45 +0530

    Delete Backend directory

--- Backend/routes/buyerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const buyerController = require("../controllers/buyerController");
-
-router.get("/products", buyerController.getAllProducts);
-router.post("/:buyerId/product/:productId/buy", buyerController.buyProduct);
-
-module.exports = router;
